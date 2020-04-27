049f38c6c55c7ec3aebdae68ee03a2028f1c5ec3
Gregory Williams <gregoryorwilliams@icloud.com>
2020-04-27 05:24:03 +0000

    Updated flash message styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  
+  # Extend default Devise gem behavior so that
+  # users signing up with the Pro account (Plan ID 2)
+  # save with a specail Stripe subscription function.
+  # Otherwise Devise signs up as per usual.
   def create
     super do |resource|
       if params[:plan]
