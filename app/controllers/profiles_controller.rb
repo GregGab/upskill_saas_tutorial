9d2d4a2bf011ded5b47b357df669d1a0cc6ec936
Gregory Williams <gregoryorwilliams@icloud.com>
2020-04-28 17:47:56 +0000

    Added add bare bones user show page and updated profiles redirect

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     @profile.save
       flash[:success] = "Profile UPdated!"
-      redirect_to root_path
+      redirect_to user_path( params[:user_id] )
   end
   
   private 
